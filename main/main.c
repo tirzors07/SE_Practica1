99882d026fe1f91d4918172c6294d32875ce7b19
Ricardo Tirzo Sotero <160087448+tirzors07@users.noreply.github.com>
2024-08-29 18:53:18 -0700

    main.c

--- main/main.c
+++ main/main.c
@@ -5,17 +5,17 @@
 #include "driver/gpio.h"
 #include "esp_timer.h"
 #include <stdio.h>
-#define BTN_1 16 // $1
-#define BTN_2 17 // $5
-#define BTN_3 22 // $10
-#define BTN_4 23 // $20
-#define PIN_LED1 5//cambio 10
-#define PIN_LED2 18//cambio 5
-#define PIN_LED3 19//cambio 1
-#define PIN_LED4 21// ticket
+#define BTN_1 16    // $1
+#define BTN_2 17    // $5
+#define BTN_3 22    // $10
+#define BTN_4 23    // $20
+#define PIN_LED1 5  // cambio 10
+#define PIN_LED2 18 // cambio 5
+#define PIN_LED3 19 // cambio 1
+#define PIN_LED4 21 // ticket
 
-//const static gpio_num_t INPUT_PINS[4] = {BTN_1,BTN_2,BTN_3,BTN_4};
-const static gpio_num_t PIN_LEDS[4] = {PIN_LED1,PIN_LED2,PIN_LED3,PIN_LED4};
+const static gpio_num_t INPUT_PINS[4] = {BTN_1, BTN_2, BTN_3, BTN_4};
+const static gpio_num_t PIN_LEDS[4] = {PIN_LED1, PIN_LED2, PIN_LED3, PIN_LED4};
 
 static uint16_t saldo = 0;
 static int64_t last_time = 0;
@@ -25,158 +25,205 @@ static uint8_t flag_ticket = 0;
 QueueHandle_t handlerQueue;
 static uint8_t flag_finishInsert = 0;
 
+typedef struct Payment
+{
+  int8_t saldo;               // saldo total
+  int64_t last_time;          // tiempo anterior
+  TaskHandle_t pxCreatedTask; // manejador de tarea
+} Payment_t;
+
 typedef enum
 {
-    Q0_COIN_WAIT = 0,
-    Q1_COIN_1_PESO = BTN_1,//btn
-    Q2_COIN_5_PESOS = BTN_2,//btn
-    Q3_COIN_10_PESOS = BTN_3,//btn
-    Q4_COIN_20_PESOS = BTN_4,//btn
-    Q5_FULL_PAYMENT = 1,//btn
-    Q6_EXCHANGE = 2
+  Q0_PAYMENT_RESTART = 0,
+  Q1_COIN_WAIT,
+  Q2_PAYMENT,
+  Q3_FULL_PAYMENT,
+  Q4_EXCHANGE
 } estado_t;
 
-estado_t current_state = Q0_COIN_WAIT;
+estado_t current_state = Q0_PAYMENT_RESTART;
+
 void gpios_init();
-// int insertarMonedas();//retorna la cantidad total de numberCoinss insertadas
-void paymentSequence(estado_t);
-void secuenciaCambio(uint8_t cambio, uint8_t piNumber); // funcion para representar cambio en leds
-void cambio(uint8_t, uint8_t, uint8_t, uint8_t);        // funcion que retorna cambio total y  utliza la func secuenciaCambio
+void insert_coins_Task(void *params); // retorna la cantidad total de numberCoinss insertadas
+void paymentSequence(estado_t *, Payment_t *payment);
+void secuenciaCambio(int8_t numberCoins, int8_t piNumber); // funcion para representar cambio en leds
+void cambio(uint8_t, uint8_t, uint8_t, uint8_t);           // funcion que retorna cambio total y  utliza la func secuenciaCambio
 void LED_Control_Task(void *params);
 static void IRAM_ATTR gpio_interrupt_handler(void *args);
 
 void app_main(void)
 {
-    //
+  Payment_t payment = {0};
+  estado_t current_state = Q1_COIN_WAIT;
+  gpios_init();
+  handlerQueue = xQueueCreate(10, sizeof(int));
+
+  // Corregimos la creación de la tarea para pasar la dirección del puntero
+  xTaskCreate(insert_coins_Task, "Insert_Coins_Task", 2048, &payment, 1, &payment.pxCreatedTask);
+
+  gpio_install_isr_service(0);
+  for (int8_t i = 0; i < 4; i++)
+  {
+    gpio_isr_handler_add(INPUT_PINS[i], gpio_interrupt_handler, (void *)INPUT_PINS[i]);
+  }
+  while (1)
+  {
+    paymentSequence(&current_state, &payment);
+    // vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
 }
 static void IRAM_ATTR gpio_interrupt_handler(void *args)
 {
-    int pinNumber = (int)args;
-    int64_t current_time = esp_timer_get_time();
-    if (((current_time - last_time) >= 150000))
-    {
-        last_time = current_time;
-        xQueueSendFromISR(handlerQueue, &pinNumber, NULL);
-    }
-}
-void LED_Control_Task(void *params)
-{
-    int pinNumber = 0, count = 0;
-    while (1)
-    {
-        if (xQueueReceive(handlerQueue, &pinNumber, portMAX_DELAY))
-        {
-            paymentSequence((estado_t)pinNumber);
-        }
-    }
+  int pinNumber = (int)args;
+  int64_t current_time = esp_timer_get_time();
+  if (((current_time - last_time) >= 150000))
+  {
+    last_time = current_time;
+    xQueueSendFromISR(handlerQueue, &pinNumber, NULL);
+  }
 }
 void cambio(uint8_t cantidad, uint8_t diez, uint8_t cinco, uint8_t un_peso)
 {
-    if (cantidad == 0)
-    {
-        secuenciaCambio(diez, PIN_LEDS[0]);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        secuenciaCambio(cinco, PIN_LEDS[1]);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        secuenciaCambio(un_peso, PIN_LEDS[2]);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-    else if (cantidad >= 10)
-    {
-        cambio(cantidad - 10, diez + 1, cinco, un_peso);
-    }
-    else if (cantidad >= 5)
-    {
-        cambio(cantidad - 5, diez, cinco + 1, un_peso);
-    }
-    else if (cantidad >= 1)
-    {
-        cambio(cantidad - 1, diez, cinco, un_peso + 1);
-    }
+  printf("Tu cambio es %d \n", cantidad);
+  if (cantidad == 0)
+  {
+    secuenciaCambio(diez, PIN_LEDS[0]);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    secuenciaCambio(cinco, PIN_LEDS[1]);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    secuenciaCambio(un_peso, PIN_LEDS[2]);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+  else if (cantidad >= 10)
+  {
+    cambio(cantidad - 10, diez + 1, cinco, un_peso);
+  }
+  else if (cantidad >= 5)
+  {
+    cambio(cantidad - 5, diez, cinco + 1, un_peso);
+  }
+  else if (cantidad >= 1)
+  {
+    cambio(cantidad - 1, diez, cinco, un_peso + 1);
+  }
 }
-void secuenciaCambio(uint8_t numberCoins, uint8_t piNumber)
+void secuenciaCambio(int8_t numberCoins, int8_t piNumber)
 {
-    for (uint8_t i = 0; i < numberCoins; i++)
-    {
-        gpio_set_level(piNumber, 1);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        gpio_set_level(piNumber, 0);
-    }
+  for (uint8_t i = 0; i < numberCoins; i++)
+  {
+    gpio_set_level(piNumber, 1);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    gpio_set_level(piNumber, 0);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
-void paymentSequence(estado_t current_state_t)
+void paymentSequence(estado_t *current_state_t, Payment_t *payment)
 {
-    uint8_t val_coin = 0;
-    current_time = esp_timer_get_time();
-    switch (current_state_t)
+  switch (*current_state_t)
+  {
+  case Q0_PAYMENT_RESTART: // INICIAL
+    printf("Hola este es el edo 0\n");
+    payment->last_time = 0;
+    payment->saldo = 0;
+    *current_state_t = Q1_COIN_WAIT;
+    break;
+  case Q1_COIN_WAIT:
+    printf("Hola este es el edo 1\n");
+    vTaskResume(payment->pxCreatedTask); // activa la tarea
+    *current_state_t = Q2_PAYMENT;
+    break;
+  case Q2_PAYMENT:
+    if ((current_time - payment->last_time) >= 2000000 && (payment->saldo > 0))
     {
-        case Q0_COIN_WAIT:
-            current_state = Q0_COIN_WAIT;
-            current_time = esp_timer_get_time();
-            time_inactivity = current_time;
-            val_coin = 0;
-        case Q1_COIN_1_PESO:
-            val_coin = 1;
-            break;
-        case Q2_COIN_5_PESOS:
-            val_coin = 5;
-            break;
-        case Q3_COIN_10_PESOS:
-            val_coin = 10;
-            break;
-        case Q4_COIN_20_PESOS:
-            val_coin = 20;
-            break;
-        case Q5_FULL_PAYMENT:
-            if((saldo-15) > 0){
-            }
-            break;
-        case Q6_EXCHANGE:
-            break;
-        default:
-            current_state = Q0_COIN_WAIT;
-            return;
+      vTaskSuspend(payment->pxCreatedTask);
+      *current_state_t = Q3_FULL_PAYMENT;
+      printf("Tiempo de inactividad agotado\n");
+      printf("Redirigiendo a pago completo con %d\n", payment->saldo);
     }
-    saldo += val_coin;
-    if ((current_time - time_inactivity) >= 200000 && (saldo > 0))
+    else
     {
-        //time_inactivity = 0;
-        current_state = Q5_FULL_PAYMENT;
+      *current_state_t = Q2_PAYMENT;
     }
+    break;
+  case Q3_FULL_PAYMENT:
+    if (payment->saldo == 15)
+    { // devolver ticket y reiniciar
+      printf("Has pagado %d\n", payment->saldo);
+      gpio_set_level(PIN_LEDS[3], 1);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+      gpio_set_level(PIN_LEDS[3], 0);
+      *current_state_t = Q0_PAYMENT_RESTART; // AL REINICIO
+    }
+    if ((payment->saldo - 15) > 0)
+    {
+      gpio_set_level(PIN_LEDS[3], 1);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+      gpio_set_level(PIN_LEDS[3], 0);
+      *current_state_t = Q4_EXCHANGE; // cambio
+    }
+    break;
+  case Q4_EXCHANGE:
+    cambio(payment->saldo, 0, 0, 0);
+    *current_state_t = Q0_PAYMENT_RESTART;
+    break;
+  default:
+    *current_state_t = Q0_PAYMENT_RESTART;
+    break;
+  }
 }
 
-/*int insertarMonedas()
+void insert_coins_Task(void *params)
 {
-    int pinNumber;
-    if (xQueueReceive(handlerQueue, &pinNumber, portMAX_DELAY))
+  Payment_t *payment = (Payment_t *)params;
+  int pinNumber;
+  while (1)
+  {
+    current_time = esp_timer_get_time();
+    if (xQueueReceive(handlerQueue, &pinNumber, portMAX_DELAY)) // entra si recibe algo
     {
-        switch (pinNumber)
-        {
-        case UN_PESO:
-            saldo++;
-            break;
-        case CINCO_PESOS:
-            saldo += 5;
-            break;
-        case DIEZ_PESOS:
-            saldo += 10;
-            break;
-        case VEINTE_PESOS:
-            saldo += 20;
-            break;
-        default:
-            break;
+      switch (pinNumber)
+      {
+      case BTN_1: // Botón 1
+        saldo++;
+        break;
+      case BTN_2: // Botón 2
+        saldo += 5;
+        break;
+      case BTN_3: // Botón 3
+        saldo += 10;
+        break;
+      case BTN_4: // Botón 4
+        saldo += 20;
+        break;
+      default:
+        break;
+      }
+      payment->last_time = current_time; // toma el ultimo valor desde que se detecto un pulso
+      payment->saldo += saldo;
+    }
+  }
+}
+/*void LED_Control_Task(void *params)
+  {
+    int pinNumber = 0, count = 0;
+    while (1){
+        if (xQueueReceive(handlerQueue, &pinNumber, portMAX_DELAY)){
+            //paymentSequence((estado_t)pinNumber);
         }
-        time_inactivity = current_time;
-        if ((current_time - time_inactivity) >= 200000 && (saldo > 0))
-        {
-            flag_finishInsert = 1;
-            // flag_ticket = 1;
-            return saldo; // aqui nose aun si es lo correcto xdd
-        }
-        else
-        {
-            flag_finishInsert = 0;
-            // flag_ticket = 0;
-        }
-    } // help me
-}*/
+    }
+  }*/
+void gpios_init()
+{
+  for (int8_t i = 0; i < 4; i++)
+  { // inicializar leds
+    gpio_reset_pin(PIN_LEDS[i]);
+    gpio_set_direction(PIN_LEDS[i], GPIO_MODE_OUTPUT);
+  }
+  for (int8_t i = 0; i < 4; i++)
+  { // botones
+    gpio_reset_pin(INPUT_PINS[i]);
+    gpio_set_direction(INPUT_PINS[i], GPIO_MODE_INPUT);
+    gpio_set_pull_mode(INPUT_PINS[i], GPIO_PULLUP_ONLY);
+    gpio_set_intr_type(INPUT_PINS[i], GPIO_INTR_NEGEDGE);
+  }
+}
